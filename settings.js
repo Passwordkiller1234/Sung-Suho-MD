76d2449754d0658da3335ca1ddb59079222b1ee1
Passwordkiller1234 <168113478+Passwordkiller1234@users.noreply.github.com>
2025-06-09 11:18:31 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "suho~oit1ibYJ#z9ByaX94rH9lADygkDOaK0QuOmnLle2i1xyQTrRJBMI",
+    SESSION_ID: process.env.SESSION_ID || "",
     // add your Session Id make sure it starts with suho~
 
     PREFIX: process.env.PREFIX || ".",
