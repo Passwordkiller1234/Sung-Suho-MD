939ff7a8cac6deb0ac3e75dc23047cba40303325
Passwordkiller1234 <168113478+Passwordkiller1234@users.noreply.github.com>
2025-06-17 22:20:29 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -103,7 +103,7 @@ module.exports = {
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
     // make true or false automatic text reply 
 
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     // make true for always online 
 
     PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
@@ -115,7 +115,7 @@ module.exports = {
     READ_CMD: process.env.READ_CMD || "false",
     // true if want mark commands as read  
 
-    DEV: process.env.DEV || "27813374457",
+    DEV: process.env.DEV || "233540190963",
     // replace with your whatsapp number  
 
     ANTI_VV: process.env.ANTI_VV || "true",
